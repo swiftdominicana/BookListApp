39de44eedd1f2c704286d6895d753bfd3a7569b3
Libranner <libranner@gmail.com>
2022-02-01 18:51:08 +0100

    Add user default checks

--- BookListApp/Services/BookService.swift
+++ BookListApp/Services/BookService.swift
@@ -13,12 +13,18 @@ import UIKit
 struct BookService {
   private let apiURL = "https://firebasestorage.googleapis.com/v0/b/fir-swift-4372e.appspot.com/o/books.json?alt=media&token=e4b68cd7-49e1-4a7a-acb3-5ec65dd043b7"
   private var context: NSManagedObjectContext
+  private let DATA_DOWNLOADED_KEY = "DATA_DOWNLOADED_KEY"
   
   init(context: NSManagedObjectContext) {
     self.context = context
   }
-  
+
   func getBooks(completion: @escaping (_ success: Bool) -> Void) {
+    if UserDefaults.standard.bool(forKey: DATA_DOWNLOADED_KEY) {
+      completion(true)
+      return
+    }
+
     guard let url = URL(string: apiURL) else {
       completion(false)
       return
@@ -39,25 +45,33 @@ struct BookService {
         return
       }
 
-      let sanitizedJson = json.compactMap { item -> [String: Any]? in
-        guard let urlString = item["coverImageUrl"] as? String,
-              let url = NSURL(string: urlString) else {
-          return nil
-        }
-
-        var copyItem = item
-        copyItem["coverImageUrl"] = url
-        return copyItem as [String: Any]
-      }
+      let sanitizedJson = sanitizeJSON(json)
 
       childContext.perform {
         let insertRequest = NSBatchInsertRequest(entity: Book.entity(), objects: sanitizedJson)
         _ = try? childContext.execute(insertRequest)
         try? childContext.save()
         completion(true)
+        UserDefaults.standard.set(true, forKey: DATA_DOWNLOADED_KEY)
       }
     }
     
     task.resume()
+  }
+}
+
+
+private extension BookService {
+  func sanitizeJSON(_ json: [[String : Any]]) -> [[String : Any]] {
+    return json.compactMap { item -> [String: Any]? in
+      guard let urlString = item["coverImageUrl"] as? String,
+            let url = NSURL(string: urlString) else {
+              return nil
+            }
+
+      var copyItem = item
+      copyItem["coverImageUrl"] = url
+      return copyItem as [String: Any]
+    }
   }
 }
